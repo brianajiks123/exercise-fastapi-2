3c87b8e9761b9369b2096817df92b84d63c3f260
Brian Aji Pamungkas <brianajiks123@gmail.com>
2024-11-26 10:54:31 +0700

    model classes and nested models

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI, HTTPException
-from enum import Enum
+from model.schemas import GenreURLChoices, Band
 
 app = FastAPI()
 
-class GenreURLChoices(Enum):
-    ROCK = 'rock'
-    ELECTRONIC = 'electronic'
-    METAL = 'metal'
-    HIP_HOP = 'hip-hop'
-
 BANDS = [
-    {'id': 1, 'name': "Brian", 'genre': 'Rock'},
+    {'id': 1, 'name': "Brian", 'genre': 'Rock', 'albums': [
+        {'title': 'Master of Hell', 'release_date': '2020-02-05'}
+    ]},
     {'id': 2, 'name': "Aji  ", 'genre': 'Pop'},
     {'id': 3, 'name': "Pamungkas", 'genre': 'Rock'},
 ]
@@ -23,13 +19,27 @@ async def home():       # by default return dict[str,str]
 async def about():      # by default return str
     return 'An Exceptional Company.'
 
+# @app.get("/bands")
+# async def bands():      # by default return list[dict]
+#     return BANDS
+
 @app.get("/bands")
-async def bands():      # by default return list[dict]
-    return BANDS
+async def bands() -> list[Band]:      # return list of Band object
+    return [
+        Band(**band) for band in BANDS
+    ]
+
+# @app.get("/bands/{band_id}")
+# async def band(band_id: int):      # by default return dict, type-hint: integer for params
+#     band = next((band for band in BANDS if band['id'] == band_id), None)
+#     if band is None:
+#         # status code 404
+#         raise HTTPException(status_code=404, details='Band not found')
+#     return band
 
 @app.get("/bands/{band_id}")
-async def band(band_id: int):      # by default return dict, type-hint: integer for params
-    band = next((band for band in BANDS if band['id'] == band_id), None)
+async def band(band_id: int) -> Band:      # return Band object, type-hint: integer for params
+    band = next((Band(**band) for band in BANDS if band['id'] == band_id), None)
     if band is None:
         # status code 404
         raise HTTPException(status_code=404, details='Band not found')
